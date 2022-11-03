c5dd5b4ad958eff453e87c35a3f83292394980d4
Verkin78 <otrajenie.jc@gmail.com>
2022-11-03 16:08:16 +0300

    Homework3.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println();
+       //task3.1//
+        int a = 1;
+        System.out.println(" Значение переменной a с типом int равно " + a);
+        byte b = 2;
+        System.out.println(" Значение переменной b с типом byte равно " + b);
+        short c = 3;
+        System.out.println(" Значение переменной c с типом short равно " + c);
+        long d = 4;
+        System.out.println(" Значение переменной d с типом long равно " + d);
+        float e = 5f;
+        System.out.println(" Значение переменной e с типом float равно " + e);
+        double f = 6;
+        System.out.println(" Значение переменной f с типом double равно " + f);
+
+
+
     }
 }
